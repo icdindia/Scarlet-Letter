14367b3abd0220320cea30667c09005c9affb0da
aj1412 <alok@icdindia.com>
2026-02-20 14:12:42 +0530

    template fix

--- pages/scarlet-letter/[slug].js
+++ pages/scarlet-letter/[slug].js
@@ -18,7 +18,7 @@ export default function newsletterss({ newsletter , slug }) {
       const element = document.getElementById('htmlContent');
       
       
-      const finalHtml = element.innerHTML;
+      const finalHtml = element.outerHTML;
       
       navigator.clipboard.writeText(finalHtml).then(() => {
         alert('Copied!');
@@ -181,10 +181,104 @@ blockquote p {
 
 h2 {
   margin: 12px 0 0;
+  font-size: 18px;
+  line-height: 25px;
+  font-family: Merriweather, Times, "Times New Roman", serif;
+  color: rgb(51, 51, 51);
 }
 
 p {
   margin: 8px 0 0;
+  font-size: 16px;
+  line-height: 27px;
+  font-family: Merriweather, Times, "Times New Roman", serif;
+  color: rgb(23, 23, 23);
+  font-weight: 400;
+}
+
+p code {
+  display: table;
+  font-style: italic;
+  font-size: 16px;
+  line-height: 24px;
+  font-family: Merriweather, Times, "Times New Roman", serif;
+  color: rgb(51, 51, 51);
+  margin-top: 22px;
+  margin-bottom: 22px;
+}
+
+li{
+  font-size: 16px;
+  line-height: 27px;
+  font-family: Merriweather, Times, "Times New Roman", serif;
+  color: rgb(23, 23, 23);
+}
+
+blockquote p em {
+    font-family: Merriweather, Times, "Times New Roman", serif !important;
+    font-size: 24px !important;
+    line-height: 31px !important;
+    color: #333 !important;
+    letter-spacing: -0.24px !important;
+    margin: 12px 0 0 !important;
+}
+
+.bottom-row{
+  display: flex !important;
+}
+
+.end-text{
+    font-family: Merriweather, Times, "Times New Roman", serif !important;
+    font-weight: 700 !important;
+    font-size: 12px !important;
+    line-height: 19px !important;
+    color: rgb(51, 51, 51) !important;
+    display: block !important;
+}
+
+.end-text-bold{
+  font-family: Merriweather, Times, "Times New Roman", serif !important;
+  font-weight: 700 !important;
+  font-size: 12px !important;
+  line-height: 19px !important;
+  color: rgb(235, 87, 87) !important;
+}
+
+.subscribe-cont p{
+  font-size: 12px !important;
+  line-height: 16px !important;
+  margin: 0px !important;
+  width: 410px !important;
+  font-family: Merriweather, Times, "Times New Roman", serif !important;
+  letter-spacing: normal !important;
+  color: rgb(23, 23, 23) !important;
+  font-weight: 400 !important;
+}
+
+.img-bottom{
+  font-family: Merriweather, Times, "Times New Roman", serif !important;
+  font-weight: 400 !important;
+  font-size: 9px !important;
+  line-height: 14px !important;
+  color: rgb(130, 130, 130) !important;
+  font-style: italic !important;
+}
+
+.lets-talk-head{
+  text-decoration: none !important;
+}
+
+.lets-talk-head span{
+  font-size: 24px;
+  line-height: 31px;
+  /* text-decoration: underline; */
+  font-style: italic;
+  font-family: Merriweather, Times, "Times New Roman", serif !important;
+  border-bottom: 1px dashed;
+}
+
+a{
+  text-decoration: none !important;
 }
 
 img {
@@ -451,7 +545,8 @@ a:visited {
   }
 
   .newsletter-container { padding: 12px !important; }
-  .newsletter-title { padding: 28px 16px 0 !important; }
+  .newsletter-title { padding: 0 !important; }
+  .letter-upper-body{padding: 0 !important;}
   .letter-text { padding: 16px !important; }
   .about-company { padding: 20px 16px !important; }
   .lets-talk-btn { padding: 0 16px 0 !important; width: 70% !important; }
@@ -478,11 +573,7 @@ a:visited {
       </head>
 
             <body className="clean-body" style={{margin: 0, padding: 0, WebkitTextSizeAdjust: '100%'}}>
-              <table bgcolor="#ffe300" cellPadding={0} cellSpacing={0} className="nl-container" role="presentation" style={{tableLayout: 'fixed', verticalAlign: 'top', minWidth: 320, margin: '0 auto', borderSpacing: 0, borderCollapse: 'collapse', msoTableLspace: '0pt', msoTableRspace: '0pt', backgroundColor: '#ff0000',  backgroundImage:
-            'url("https://images.ctfassets.net/d3a4e93836in/1kdZ2WvHYl9jF2zRer42LU/9f247405542b9f0e9b835382084eb20c/Desktop_-_6.png")',
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover", width: '100%'}} valign="top" width="100%">
+              <table background="https://images.ctfassets.net/d3a4e93836in/1kdZ2WvHYl9jF2zRer42LU/9f247405542b9f0e9b835382084eb20c/Desktop_-_6.png"  cellPadding={0} cellSpacing={0} className="nl-container" role="presentation" style={{tableLayout: 'fixed', verticalAlign: 'top', minWidth: 320, margin: '0 auto', borderSpacing: 0, borderCollapse: 'collapse', msoTableLspace: '0pt', msoTableRspace: '0pt',  width: '100%'}} valign="top" width="100%">
                 <tbody>
                   <tr style={{verticalAlign: 'top'}} valign="top">
                     <td style={{wordBreak: 'break-word', verticalAlign: 'top' , padding: '20px 0 90px 0'}} valign="top" className="newsletter-container">
@@ -491,7 +582,7 @@ a:visited {
                           <div style={{borderCollapse: 'collapse', display: 'table', width: '100%', backgroundColor: 'transparent'}}>
                                       <div className="col num6 title-cont" style={{maxWidth: 320, minWidth: 300, display: 'table-cell', verticalAlign: 'top', width: 300}}>
                                         <div style={{width: '100% !important'}}>
-                                          <div style={{borderTop: '0px solid transparent', borderLeft: '0px solid transparent', borderBottom: '0px solid transparent', borderRight: '0px solid transparent', paddingTop: 0, paddingBottom: 5, paddingRight: 0, paddingLeft: 0}}>
+                                          <div style={{borderTop: '0px solid transparent', borderLeft: '0px solid transparent', borderBottom: '0px solid transparent', borderRight: '0px solid transparent', paddingTop: 40, paddingBottom: 5, paddingRight: 30, paddingLeft: 30}}>
                                             <div style={{color: '#171717', fontFamily: '-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen-Sans,Ubuntu,Cantarell,Helvetica Neue,sans-serif', lineHeight: '1.2', paddingTop: 40, paddingRight: 30, paddingBottom: 0, paddingLeft: 30}} className="newsletter-title">
                                               <div style={{fontFamily: '-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen-Sans,Ubuntu,Cantarell,Helvetica Neue,sans-serif', fontSize: 12, lineHeight: '1.2', color: '#171717', msoLineHeightAlt: 14}}>
                                                 <p style={{fontSize: 38, lineHeight: 1, msoLineHeightAlt: 46, margin: 0}}>
@@ -528,7 +619,7 @@ a:visited {
                       <div className="block-grid" style={{margin: '0 auto', minWidth: 320, maxWidth: 650, overflowWrap: 'break-word', wordWrap: 'break-word', wordBreak: 'break-word'}}>
                         <div className="col num12" style={{minWidth: 320, maxWidth: 650, display: 'table-cell', verticalAlign: 'top', width: 650 , background: '#F4F1EA'}}>
                           <div style={{width: '100% !important'}}>
-                            <div style={{borderTop: '0px solid transparent', borderLeft: '0px solid transparent', borderBottom: '0px solid transparent', borderRight: '0px solid transparent', paddingTop: 0, paddingBottom: 0, paddingRight: 0, paddingLeft: 0}}> 
+                            <div className='letter-upper-body' style={{borderTop: '0px solid transparent', borderLeft: '0px solid transparent', borderBottom: '0px solid transparent', borderRight: '0px solid transparent', paddingTop: 30, paddingBottom: 0, paddingRight: 30, paddingLeft: 30}}> 
                               <div className="letter-text" style={{fontFamily: '-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen-Sans,Ubuntu,Cantarell,Helvetica Neue,sans-serif', lineHeight: '1.2', paddingTop: 30, paddingRight: 30, paddingBottom: 0, paddingLeft: 30 , background:'#F4F1EA'}}>
                                 <div style={{fontFamily: 'Merriweather,Times,Times New Roman,serif', fontSize: 16, lineHeight: 25+'px', color: '#171717 !important'}}>
                                   <div className='email-container'>
@@ -548,15 +639,15 @@ a:visited {
                               <div style={{width: '100% !important'}}>
                                 <div className="about-company midd-padding-zero" style={{borderTop: '0px solid transparent', borderLeft: '0px solid transparent', borderBottom: '0px solid transparent', borderRight: '0px solid transparent', paddingTop: 0, paddingBottom: 16, paddingRight: 30, paddingLeft: 30, background: '#F4F1EA'}}>
                                   <div className="about-company-text" style={{color: '#171717', fontFamily: '-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif', lineHeight: '1.2', paddingTop: 0, paddingBottom: 0}}>
-                                    <div style={{fontFamily: '-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen-Sans,Ubuntu,Cantarell,Helvetica Neue,sans-serif', fontSize: 12, lineHeight: '1.2', color: '#171717', msoLineHeightAlt: 14 , display: 'flex'}}>
+                                    <div className='bottom-row' style={{fontFamily: '-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen-Sans,Ubuntu,Cantarell,Helvetica Neue,sans-serif', fontSize: 12, lineHeight: '1.2', color: '#171717', msoLineHeightAlt: 14 , display: 'flex'}}>
                                       <span className='first-child'>
                                         <img loading="lazy" decoding="async" align="right" alt="Image" border={0} className="right autowidth" src="https://images.ctfassets.net/da6fwo03rafe/5W8FPsTIefK1xGAGObOLeA/d14f00352fe7335cdc84ff3e2c69fb8d/Frame_2147263006.png" style={{textDecoration: 'none', msInterpolationMode: 'bicubic', height: 'auto', border: 'none', width: '88', maxWidth: 88, maxHeight: 40, height: '40', float: 'none', display: 'block'}} title="Image" width={172} />
-                                        <span style={{fontFamily: 'Merriweather,Times,Times New Roman,serif', fontWeight: '400', fontSize: 9, lineHeight: 14+'px' , color: '#828282', fontStyle: 'italic'}}>midjourney + me</span>
+                                        <span className='img-bottom' style={{fontFamily: 'Merriweather,Times,Times New Roman,serif', fontWeight: '400', fontSize: 9, lineHeight: 14+'px' , color: '#828282', fontStyle: 'italic'}}>midjourney + me</span>
                                       </span>
                                       <img loading="lazy" decoding="async" align="right" alt="Image" border={0} className="right mid-child autowidth" src="https://images.ctfassets.net/da6fwo03rafe/6zjXSKPxiI9MFpO89ejJ3E/561351b9e7e696811db8d759d16a2bf0/Frame_2147263000.png" style={{textDecoration: 'none', msInterpolationMode: 'bicubic', height: 'auto', border: 'none', width: '180', maxWidth: 180, float: 'none', display: 'block' , margin: '0 100px'}} title="Image" width={172} />
                                         <div className='last-child'>
-                                          <span style={{fontFamily: 'Merriweather,Times,Times New Roman,serif', fontWeight: '700', fontSize: 12, lineHeight: 19+'px' , color: '#333' , display: 'block'}}>Why call it</span>
-                                          <span style={{fontFamily: 'Merriweather,Times,Times New Roman,serif', fontWeight: '700', fontSize: 12, lineHeight: 19+'px' , color: '#EB5757'}}>The Scarlet Letter</span>
+                                          <span className='end-text' style={{fontFamily: 'Merriweather,Times,Times New Roman,serif', fontWeight: '700', fontSize: 12, lineHeight: 19+'px' , color: '#333' , display: 'block'}}>Why call it</span>
+                                          <span className='end-text end-text-bold' style={{fontFamily: 'Merriweather,Times,Times New Roman,serif', fontWeight: '700', fontSize: 12, lineHeight: 19+'px' , color: '#EB5757'}}>The Scarlet Letter</span>
                                           <a href="https://icd-india.com/" tabIndex={-1} target="_blank" style={{display: 'block' , marginTop: 4}}> 
                                             <span style={{fontFamily: 'Merriweather,Times,Times New Roman,serif', fontWeight: '400', fontSize: 9, lineHeight: 14+'px' , color: '#333' , letterSpacing: 0.54+'px'}}>READ HERE</span>
                                           </a>
@@ -592,24 +683,22 @@ a:visited {
                       <div style={{backgroundColor: 'transparent'}}>
                         <div className="block-grid two-up" style={{margin: '0 auto', minWidth: 320, maxWidth: 650, overflowWrap: 'break-word', wordWrap: 'break-word', wordBreak: 'break-word', backgroundColor: '#F4F1EA'}}>
                           <div style={{borderCollapse: 'collapse', display: 'table', width: '100%', backgroundColor: 'transparent'}}>
-                            <div className="col num6" style={{ display: 'table-cell', verticalAlign: 'top'}}>
-                        <div className="block-grid" style={{margin: '0 auto',overflowWrap: 'break-word', wordWrap: 'break-word', wordBreak: 'break-word', backgroundColor: 'transparent'}}>
-                          <div style={{borderCollapse: 'collapse', display: 'table', width: '100%', backgroundColor: 'transparent'}}>
-                            <div className="col num12" style={{display: 'table-cell', verticalAlign: 'top'}}>
-                              <div style={{width: '100% !important'}}>
-                                <div className="lets-talk-btn" style={{borderTop: '0px solid transparent', borderLeft: '0px solid transparent', borderBottom: '0px solid transparent', borderRight: '0px solid transparent', paddingTop: 0, paddingBottom: 13, paddingRight: 30, paddingLeft: 30, background: '#F4F1EA'}}>
-                                  <div align="left" className="button-container" style={{paddingTop: 0, paddingRight: 0, paddingBottom: 0, paddingLeft: 0}}>
-                                    <a href="https://www.icdindia.com/contact/" style={{WebkitTextSizeAdjust: 'none', textDecoration: 'none', display: 'inline-block', color: '#171717', backgroundColor: 'transparent', borderRadius: 4, WebkitBorderRadius: 0, MozBorderRadius: 4, width: 'auto', borderTop: '1px solid transparent', borderRight: '1px solid transparent', borderBottom: '1px dotted #171717', borderLeft: '1px solid transparent', paddingTop: 0, paddingBottom: 0, fontFamily: 'Merriweather,Times,Times New Roman,serif', textAlign: 'center', msoBorderAlt: 'none', wordBreak: 'keep-all'}} target="_blank">
-                                      <span style={{paddingLeft: 0, paddingRight: 0, fontSize: 16, display: 'inline-block'}}>
-                                        <span style={{fontSize: 24, lineHeight: 31+'px', msoLineHeightAlt: 32}}>Let’s talk.</span>
-                                      </span>
-                                    </a>
+                            <div className="col num6" style={{ display: 'table-cell', verticalAlign: 'top' , width: '36%'}}>
+                              <div className="block-grid" style={{margin: '0 auto',overflowWrap: 'break-word', wordWrap: 'break-word', wordBreak: 'break-word', backgroundColor: 'transparent'}}>
+                                <div style={{borderCollapse: 'collapse', display: 'table', width: '100%', backgroundColor: 'transparent'}}>
+                                  <div className="col num12" style={{display: 'table-cell', verticalAlign: 'top'}}>
+                                    <div style={{width: '100% !important'}}>
+                                      <div className="lets-talk-btn" style={{borderTop: '0px solid transparent', borderLeft: '0px solid transparent', borderBottom: '0px solid transparent', borderRight: '0px solid transparent', paddingTop: 0, paddingBottom: 13, paddingRight: 30, paddingLeft: 30, background: '#F4F1EA'}}>
+                                        <div align="left" className="button-container" style={{paddingTop: 0, paddingRight: 0, paddingBottom: 0, paddingLeft: 0}}>
+                                          <a className="lets-talk-head" href="https://www.icdindia.com/contact/" style={{WebkitTextSizeAdjust: 'none', textDecoration: 'none', display: 'inline-block', color: '#171717', backgroundColor: 'transparent', borderRadius: 4, WebkitBorderRadius: 0, MozBorderRadius: 4, width: 'auto', borderTop: '1px solid transparent', borderRight: '1px solid transparent', borderBottom: '1px dotted #171717', borderLeft: '1px solid transparent', paddingTop: 0, paddingBottom: 0, fontFamily: 'Merriweather,Times,Times New Roman,serif', textAlign: 'center', msoBorderAlt: 'none', wordBreak: 'keep-all'}} target="_blank">
+                                              <span style={{fontSize: 24, lineHeight: 31+'px', msoLineHeightAlt: 32}}>Let’s talk.</span>
+                                          </a>
+                                        </div>
+                                      </div>
+                                    </div>
                                   </div>
                                 </div>
                               </div>
-                            </div>
-                          </div>
-                        </div>
                               <div style={{width: '100% !important'}}>
                                 <div style={{borderTop: '0px solid transparent', borderLeft: '0px solid transparent', borderBottom: '0px solid transparent', borderRight: '0px solid transparent', paddingTop: 0, paddingBottom: 0, paddingRight: 0, paddingLeft: 0}}>
                                   <table cellPadding={0} cellSpacing={0} className="social_icons" role="presentation" style={{tableLayout: 'fixed', verticalAlign: 'top', borderSpacing: 0, borderCollapse: 'collapse', msoTableLspace: '0pt', msoTableRspace: '0pt'}} valign="top" width="100%">
@@ -632,7 +721,7 @@ a:visited {
                                 </div>
                               </div>
                             </div>
-                            <div className="col num6" style={{display: 'table-cell', verticalAlign: 'top',}}>
+                            <div className="col num6" style={{display: 'table-cell', verticalAlign: 'top', paddingRight: 30}}>
                               <div style={{width: '100% !important'}}>
                                 <div style={{borderTop: '0px solid transparent', borderLeft: '0px solid transparent', borderBottom: '0px solid transparent', borderRight: '0px solid transparent', paddingTop: 0, paddingBottom: 5, paddingRight: 0, paddingLeft: 0}}>
                                   <div className="subscribe-cont" style={{color: '#171717', fontFamily: '-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen-Sans,Ubuntu,Cantarell,Helvetica Neue,sans-serif', lineHeight: '1.2', paddingTop: 0, paddingRight: 30, paddingBottom: 0, paddingLeft: 0}}>
